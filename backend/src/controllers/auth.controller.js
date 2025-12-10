38a2c23e27b5cb735ae61540538b8a0378604d94
Test-demo <priya@redfernstech.com>
2025-12-10 13:34:55 +0530

    Welcome email implemented

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,6 +1,9 @@
 import { generateToken } from "../lib/utils.js";
+import { sendWelcomeEmail } from "../emails/emailHandlers.js";
 import User from "../models/User.js";
 import bcrypt from "bcryptjs";
+import { ENV } from "../lib/env.js";
+
 
 export const signup = async (req, res) => {
     const { fullName, email, password } = req.body;
@@ -50,10 +53,18 @@ export const signup = async (req, res) => {
             email: savedUser.email,
             profilePic: savedUser.profilePic
         });
+   
+        try {
+            await sendWelcomeEmail(savedUser.email, savedUser.fullName, ENV.CLIENT_URL);
+        } catch (error) {
+            console.error("Failed to send welcome email:", error);
+        }
 
+   
     } catch (error) {
-        console.error("Error in signup controller:", error);
+        console.log("Error in signup controller:", error);
         res.status(500).json({ message: "Internal server error" });
     }
+
 };
 
