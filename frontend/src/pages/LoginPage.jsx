934342fb8748c14e3b9878f7bbbc025efe208b49
Test-demo <priya@redfernstech.com>
2025-12-18 16:21:55 +0530

    frontend codebase setup

---
+++ frontend/src/pages/LoginPage.jsx
@@ -0,0 +1,7 @@
+
+function LoginPage() {
+  const { autUser, isLoading, login } = useAuthStore();
+  return <div>LoginPage</div>;
+}
+
+export default LoginPage;
---
+++ frontend/src/pages/SignUpPage.jsx
@@ -0,0 +1,6 @@
+function SignPage() {
+  const { autUser, isLoading, login } = useAuthStore();
+  return <div>SignPage</div>;
+}
+
+export default SignPage;
