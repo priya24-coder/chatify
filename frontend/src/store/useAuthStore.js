5fb99278dbe29bb53f98bd527310dbec07d4cd1d
Test-demo <priya@redfernstech.com>
2025-12-22 20:58:04 +0530

    login & Logout implemented

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -6,6 +6,7 @@ export const useAuthStore = create((set) => ({
   authUser: null,
   isCheckingAuth: true,
   isSigningUp: false,
+  isLoggingIn: false,
 
   checkAuth: async () => {
     try {
@@ -30,6 +31,33 @@ export const useAuthStore = create((set) => ({
       toast.error(error.response?.data?.message);
     } finally {
       set({ isSigningUp: false });
+    }
+  },
+
+  login: async (data) => {
+    set({ isLoggingIn: true });
+
+    try {
+      const res = await axiosInstance.post("/auth/login", data);
+      set({ authUser: res.data });
+
+      toast.success("Logged in successfully");
+    } catch (error) {
+      toast.error(error.response?.data?.message);
+    } finally {
+      set({ isLoggedIn: false });
+    }
+  },
+
+  logout: async () => {
+    try {
+      const res = await axiosInstance.post("/auth/logout");
+      set({ authUser: null });
+
+      toast.success("Logged Out successfully");
+    } catch (error) {
+      toast.error("Error logging Out");
+    console.log("Logout error:, error");
     }
   },
 }));
