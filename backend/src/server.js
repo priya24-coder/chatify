b6d316573e572f6017d30a0d80ff1dda19f5a87f
Test-demo <priya@redfernstech.com>
2026-01-07 20:11:34 +0530

    online users implementated

--- backend/src/server.js
+++ backend/src/server.js
@@ -12,9 +12,9 @@ import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
 import { connectDB } from "./lib/db.js";
 import { ENV } from  "./lib/env.js";
+import { app, server } from "./lib/socket.js";
 
 
-const app = express();
 const __dirname = path.resolve();
 
 const PORT = ENV.PORT || 3000;
@@ -37,7 +37,7 @@ if (ENV.NODE_ENV === "production") {
 }
 
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
 
     console.log("Server running on port: " +  PORT);
         connectDB();
--- frontend/src/components/ChatHeader.jsx
+++ frontend/src/components/ChatHeader.jsx
@@ -5,6 +5,8 @@ import { useAuthStore } from "../store/useAuthStore";
 
 function ChatHeader() {
     const { selectedUser, setSelectedUser } = useChatStore();
+    const { onlineUsers } = useAuthStore();
+    const isOnline = onlineUsers.includes(selectedUser._id);
 
    useEffect(() => {
     const handleEscKey = (event) => {
@@ -22,7 +24,7 @@ function ChatHeader() {
     className="flex justify-between items-center bg-slate-800/50 border-b border-slate-700/50 max-h-[84px] px-6 flex-1">
 
         <div className="flex items-center space-x-3">
-            <div className="avatar online">
+            <div className={`avatar ${isOnline ? "online" : "offline"}`}>
                 <div className="w-12 rounded-full">
                     <img src={selectedUser.profilePic || "/avatar-r.png"} alt={selectedUser.fullName} />
 
@@ -31,7 +33,7 @@ function ChatHeader() {
 
             <div>
                 <h3 className="text-slate-200 font-medium">{selectedUser.fullName}</h3>
-                <p className="text-slate-400 text-sm">Online</p>
+                <p className="text-slate-400 text-sm">{isOnline ? "online" : "offline"}</p>
             </div>
         </div>  
 
