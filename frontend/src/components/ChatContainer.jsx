825cd6b364b411fb18ff22c915081d0e89cf5ef0
Test-demo <priya@redfernstech.com>
2026-01-08 13:42:10 +0530

    realtime message implementated

--- frontend/src/components/ChatContainer.jsx
+++ frontend/src/components/ChatContainer.jsx
@@ -7,17 +7,26 @@ import MessageInput from "./MessageInput";
 import MessagesLoadingSkeleton from "./MessagesLoadingSkeleton";
 
 function ChatContainer() {
-  const { selectedUser, getMessageByUserId, messages, isMessagesLoading } = useChatStore();
+  const {
+    selectedUser,
+    getMessagesByUserId,
+    messages,
+    isMessagesLoading,
+    subscribeToMessages,
+    unsubscribeFromMessages,
+  } = useChatStore();
   const { authUser } = useAuthStore();
   const messageEndRef = useRef(null);
 
   useEffect(() => {
-    if (selectedUser?._id) {
-      getMessageByUserId(selectedUser._id);
-    }
-  }, [selectedUser, getMessageByUserId]);
+    getMessagesByUserId(selectedUser._id);
+    subscribeToMessages();
 
-  useEffect(() => {    //scroll to bottom when new message comes
+    // clean up
+    return () => unsubscribeFromMessages();
+  }, [selectedUser, getMessagesByUserId, subscribeToMessages, unsubscribeFromMessages]);
+
+  useEffect(() => {
     if (messageEndRef.current) {
       messageEndRef.current.scrollIntoView({ behavior: "smooth" });
     }
@@ -26,18 +35,13 @@ function ChatContainer() {
   return (
     <>
       <ChatHeader />
-
-      <div className="flex-1 px-6 py-8 overflow-y-auto">
-        {isMessagesLoading ? (
-          <MessagesLoadingSkeleton />
-        ) : messages.length > 0 ? (
+      <div className="flex-1 px-6 overflow-y-auto py-8">
+        {messages.length > 0 && !isMessagesLoading ? (
           <div className="max-w-3xl mx-auto space-y-6">
             {messages.map((msg) => (
               <div
                 key={msg._id}
-                className={`chat ${
-                  msg.senderId === authUser._id ? "chat-end" : "chat-start"
-                }`}
+                className={`chat ${msg.senderId === authUser._id ? "chat-end" : "chat-start"}`}
               >
                 <div
                   className={`chat-bubble relative ${
@@ -47,17 +51,11 @@ function ChatContainer() {
                   }`}
                 >
                   {msg.image && (
-                    <img
-                      src={msg.image}
-                      alt="shared"
-                      className="rounded-lg h-48 object-cover"
-                    />
+                    <img src={msg.image} alt="Shared" className="rounded-lg h-48 object-cover" />
                   )}
-
                   {msg.text && <p className="mt-2">{msg.text}</p>}
-
-                  <p className="text-xs mt-1 opacity-75">
-                    {new Date(msg.createdAt).toLocaleTimeString(undefined, {  //update the local time format
+                  <p className="text-xs mt-1 opacity-75 flex items-center gap-1">
+                    {new Date(msg.createdAt).toLocaleTimeString(undefined, {
                       hour: "2-digit",
                       minute: "2-digit",
                     })}
@@ -65,14 +63,13 @@ function ChatContainer() {
                 </div>
               </div>
             ))}
-
-        <div ref={messageEndRef}/>  {/*chat scroll to bottom*/}
-
+            {/* 👇 scroll target */}
+            <div ref={messageEndRef} />
           </div>
+        ) : isMessagesLoading ? (
+          <MessagesLoadingSkeleton />
         ) : (
-          selectedUser && (
-            <NoChatHistoryPlaceholder name={selectedUser.fullName} />
-          )
+          <NoChatHistoryPlaceholder name={selectedUser.fullName} />
         )}
       </div>
 
--- frontend/src/hooks/useKeyboardSound.js
+++ frontend/src/hooks/useKeyboardSound.js
@@ -1,19 +1,19 @@
 const keyStrokeSounds = [
-    new Audio("/Sounds/keystroken1.mp3"),
-    new Audio("/Sounds/keystroken2.mp3"),
-    new Audio("/Sounds/keystroken3.mp3"),
-    new Audio("/Sounds/keystroken4.mp3"),
+  new Audio("/sounds/keystroke1.mp3"),
+  new Audio("/sounds/keystroke2.mp3"),
+  new Audio("/sounds/keystroke3.mp3"),
+  new Audio("/sounds/keystroke4.mp3"),
 ];
 
-function usekeyboardSound() {
-    const playRandomKeyStrokeSound = () => {
-      const randomSound = keyStrokeSounds[Math.floor(Math.random() * keyStrokeSounds.length)];
+function useKeyboardSound() {
+  const playRandomKeyStrokeSound = () => {
+    const randomSound = keyStrokeSounds[Math.floor(Math.random() * keyStrokeSounds.length)];
 
-      randomSound.currentTime = 0; //this is for a vetter UX,
-      randomSound.play().catch((error) => console.log("Audio play failed:", error));
-        };
+    randomSound.currentTime = 0; // this is for a better UX, def add this
+    randomSound.play().catch((error) => console.log("Audio play failed:", error));
+  };
 
-    return {playRandomKeyStrokeSound}
+  return { playRandomKeyStrokeSound };
 }
 
-export default usekeyboardSound;
+export default useKeyboardSound;
